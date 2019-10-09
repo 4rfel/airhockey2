5547ab6b145924739c3f615de9237c6ae59533e0
Arfel <rafael_1999_@hotmail.com.br>
2019-10-09 17:08:49 -0300

    Create cpu_stricker_class.py

---
+++ cpu_stricker_class.py
@@ -0,0 +1,8 @@
+from stricker import Stricker
+
+class CPU_stricker(Stricker):
+    def __init__(self, sprite, center, size):
+        super().__init__(sprite, center, size)
+
+    def move(self):
+        pass
