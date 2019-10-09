0fc9e2b33929327778eb8d0d0a5d31b8d3bc1f76
Arfel <rafael_1999_@hotmail.com.br>
2019-10-09 19:22:20 -0300

    Update table_class.py

--- table_class.py
+++ table_class.py
@@ -1,12 +1,16 @@
 from object_class import Basic_object
 
 class Table(Basic_object):
-    def __init__(self, sprite, center, size, goal1, goal2):
+    def __init__(self, sprite, center, size, player_goal, CPU_goal):
         super().__init__(sprite, center, size)
-        self.goal1 = goal1
-        self.goal2 = goal2
+        self.player_goal   = player_goal # [x1, y1, x2, x2]
+        self.CPU_goal      = CPU_goal # [x1, y1, x2, x2]
         self.player_points = 0
-        self.CPU_points = 0
+        self.CPU_points    = 0
 
     def score(self, disk):
-        pass
+        center = disk.get_center()
+        if self.player_goal[0] > center[0] > self.player_goal[2] and self.player_goal[1] > center[1] > self.player_goal[3]:
+            self.CPU_points += 1
+        if self.CPU_goal[0] > center[0] > self.CPU_goal[2] and self.CPU_goal[1] > center[1] > self.player_goal[3]:
+            self.player_goal += 1
